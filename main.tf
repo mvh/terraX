ab4971c3441394ad8258b82bbbcb216ada6b8256
Michael Harding <mike@truex.com>
2017-08-18 12:27:10 -0700

    Drop down to one module.

--- main.tf
+++ main.tf
@@ -7,11 +7,11 @@ module "foo" {
   subnet = "10.1.0.0/24"
 }
 
-module "boo" {
-  source = "module"
-  tag_name = "hello world"
-  cluster_name = "boo"
-  region = "us-east-2"
-  cidr_block = "10.2.0.0/16"
-  subnet = "10.2.0.0/24"
-}
+#module "boo" {
+#  source = "module"
+#  tag_name = "hello world"
+#  cluster_name = "boo"
+#  region = "us-east-2"
+#  cidr_block = "10.2.0.0/16"
+#  subnet = "10.2.0.0/24"
+#}
